6c7f9a65320a028790ac0728362fac9d95e7c2b0
randomaccount <87091634+yxkal999YT@users.noreply.github.com>
2026-06-26 13:40:05 -0400

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6Ijk1MTMyZGI4LWY1MWItNGUxZS1iYjc2LTA1ZTZlNzI1NTkyZCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/JnlbNfI6ghkZN3v8/raw"))()
